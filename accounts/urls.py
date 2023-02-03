1e6df8d4aee478f5a7de04853778ee0f1c9fa4d9
GaoxingAbdullah <abduldoobia@yahoo.com>
2023-02-04 03:51:42 +0800

    Reset password email added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
@@ -19,4 +21,17 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerUser, name='register'),
+    
+    
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), 
+         name="reset_password"),
+    
+    path('reset_password_sent/)', auth_views.PasswordChangeDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+         name="password_reset_sent"),
+    
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
+         name="password_reset_confirm"),
+    
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"), 
+         name="password_reset_complete"),
 ]
--- crm/settings.py
+++ crm/settings.py
@@ -135,3 +135,22 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#SMTP Configuration 
+
+#for gmail 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'your email'
+# EMAIL_HOST_PASSWORD = 'your password'
+
+#for yahoo mail
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.mail.yahoo.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = "your email"
+# EMAIL_HOST_PASSWORD = 'your password'
