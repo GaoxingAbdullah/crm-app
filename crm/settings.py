14a4f014793260844a13426f606600bad4685789
GaoxingAbdullah <abduldoobia@yahoo.com>
2023-02-06 16:51:10 +0800

    Change database to postgres

--- crm/settings.py
+++ crm/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crm',
+        'USER': 'postgres',
+        'PASSWORD': 'Abdallah2014@',
+        'HOST': 'localhost',
+        'POST': "5432"
     }
 }
 
